a3420c954e9618ddc39ac1bb9bbf4dc92894100a
peter <peter.lm.schmidt@gmail.com>
2021-10-16 18:13:13 +0200

    .

--- 3._Dank_Memes/app.js
+++ 3._Dank_Memes/app.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 
 const dankMemes = [{
+    id: 1,
     topText: "Mom: We have Javascript at home",
     bottomText: "Javascript at home: Node.js"
 }];
@@ -13,30 +14,43 @@ app.get("/dankmemes", (req, res) => {
 
 app.get("/dankmemes/:id", (req, res) => {
     res.send({memes: dankMemes[req.params.id]});
+    //error handling hvis id ikke findes:
+    // const foundMeme = dankMemes.find(dankMeme => dankMeme.id === Number(req.params.id));
+    // foundMeme ? res.send(foundMeme) : res.sendStatus(404);
 });
 
 app.post("/dankmemes/post", (req, res) => {
     dankMemes.push(req.body);
-    res.send(req.body);
+    res.sendStatus(200);
 });
 
 app.delete("/dankmemes/del", (req, res) => {
     dankMemes.pop();
-    res.send({memes: dankMemes});
+    res.sendStatus(200);
 })
 
 app.delete("/dankmemes/del/:id", (req, res) => {
+    // const foundMeme = dankMemes.find(dankMeme => dankMeme.id === Number(req.params.id));
+    // foundMeme ? dankMemes.splice(req.params.id, 1); : res.sendStatus(404);
     dankMemes.splice(req.params.id, 1);
-    res.send({memes: dankMemes});
+    res.sendStatus(200);
 });
 
 app.put("/dankmemes/put/:id", (req, res) => {
-    dankMemes[req.params.id] = req.body;
-    res.send({memes: dankMemes});
+    dankMemes.find(dankMeme => dankMeme.id === Number(req.params.id)) = req.body;
+    res.sendStatus(200);
 });
 
 app.patch("/dankmemes/patch:/id", (req, res) => {
-
+    let memeToUpdate;
+    dankMemes = dankMemes.map(dankMeme => {
+        if (dankMeme.id === Number(req.params.id)) {
+            memeToUpdate = { ...dankMeme, ...req.body, id: dankMeme.id };
+            return memeToUpdate;
+        }
+        return dankMeme;
+    });
+    memeToUpdate ? res.send(memeToUpdate): res.sendStatus(404);
 });
 
 app.listen(8080, (error) => {
